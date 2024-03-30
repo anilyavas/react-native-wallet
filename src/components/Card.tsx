d7e0835cc2410aeb0ad9cbe8953933c2ff9779fa
anilyavas <anil.yavas99@gmail.com>
2024-03-30 14:20:45 +0300

    Stacking cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import Animated, { useDerivedValue } from 'react-native-reanimated';
+import Animated, { clamp, useDerivedValue } from 'react-native-reanimated';
 
 const Card = ({ card, index, scrollY }) => {
-  const translateY = useDerivedValue(() => -scrollY.value);
+  const [cardHeight, setCardHeight] = useState(0);
+  const translateY = useDerivedValue(() =>
+    clamp(-scrollY.value, -index * cardHeight, 0)
+  );
   return (
     <Animated.Image
       source={card}
+      onLayout={(event) => setCardHeight(event.nativeEvent.layout.height + 10)}
       style={[styles.image, { transform: [{ translateY: translateY }] }]}
     />
   );
